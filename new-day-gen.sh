8d946fb891ccfa326a65fb5c34e38d4e46d2b478
Luis Morillo <luis_cnnvd@hotmail.com>
2022-12-04 11:15:58 +0100

    better main

--- new-day-gen.sh
+++ new-day-gen.sh
@@ -4,13 +4,21 @@ d=day-${1}
 
 module_header="module Main where
 
+import System.Environment (getArgs)
+
 main :: IO ()
 main = do
-  putStrLn \"Solution to part 1 is:\"
-  print $ \"part one not implemented\"
-  putStrLn \"Solution to part 2 is:\"
-  print $ \"part two not implemented\"
+  [part, filepath] <- getArgs
+  input <- lines <$> readFile filepath
+  if read @Int part == 1
+    then do
+      print \"solution to problem 1 is:\"
+      print \"not implemented\"
+    else do
+      print \"solution to problem 2 is:\"
+      print \"not implemented\"
 "
+
 
 cabal_day="executable ${d}
   main-is: ${d}.hs
